c1e6447966eef0cd32a54ff2392431218c5482f4
Linda Wu <linda.wu723@gmail.com>
2025-03-26 11:44:23 -0700

    Add smooth scroll with Lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Lenis from 'lenis'
 
 const App = () => {
+
+  React.useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+        autoRaf: true,
+    });
+    
+    // Listen for the scroll event and log the event data
+    lenis.on('scroll', (e) => {
+        console.log(e);
+    });
+
+}, []);
+
   return (
     <Router>
       <Routes>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,7 +5,6 @@ import Box from '@mui/material/Box';
 
 
 const Home = () => { 
-
     return (
         <>
             <Header />
