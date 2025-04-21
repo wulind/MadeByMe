b6e9c5a86cd537f104edeac0cd4f92c8111cf9ad
Linda Wu <linda.wu723@gmail.com>
2025-04-21 13:54:55 -0700

    clean dev dependencies

--- client/src/components/pages/home/Effect.tsx
+++ client/src/components/pages/home/Effect.tsx
@@ -1,5 +1,4 @@
 import { extend, useThree } from "@react-three/fiber";
-// import { useControls } from "leva";
 import { useEffect, useRef } from "react";
 import { EffectComposer, RenderPass, ShaderPass } from "three-stdlib";
 
@@ -9,11 +8,6 @@ import { useRipplePass } from "./postprocessing/RipplePass";
 extend({ EffectComposer, RenderPass, ShaderPass });
 
 export const Effect = () => {
-  // const dist_datas = useControls("Distortion", {
-  //   progress: { value: 1, min: 0, max: 1, step: 0.01 },
-  //   scale: { value: 1, min: 0, max: 1, step: 0.01 },
-  // });
-
   const composer = useRef<EffectComposer>(null);
   const distortionPass = useRef(useDistortionPass());
 
